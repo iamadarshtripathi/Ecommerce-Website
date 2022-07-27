6e5277f0095f0cb873d705787277edd7c5326608
Adarsh Tripathi <adarsh29tripathi@gmail.com>
2022-07-27 18:23:53 +0530

    file change

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export const Connection = async () => {
-    const URL = 'mongodb://user:addes24@ecommerce-web-shard-00-00.wz82z.mongodb.net:27017,ecommerce-web-shard-00-01.wz82z.mongodb.net:27017,ecommerce-web-shard-00-02.wz82z.mongodb.net:27017/PROJECT?ssl=true&replicaSet=atlas-xu5s6u-shard-0&authSource=admin&retryWrites=true&w=majority';
+    const URL = 'mongodb://username:password@ecommerce-web-shard-00-00.wz82z.mongodb.net:27017,ecommerce-web-shard-00-01.wz82z.mongodb.net:27017,ecommerce-web-shard-00-02.wz82z.mongodb.net:27017/PROJECTname?ssl=true&replicaSet=atlas-xu5s6u-shard-0&authSource=admin&retryWrites=true&w=majority';
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
         console.log('Database Connected Succesfully');
